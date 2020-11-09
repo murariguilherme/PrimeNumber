1be65778f741c88a3ec9affaf673b75e81336308
Guilherme Murari <guilherme.murari@moondu.io>
2020-11-09 19:24:41 +0000

    initial controller

---
+++ src/PrimeNumber.Api/Controllers/PrimeNumberController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeNumber.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrimeNumberController : ControllerBase
+    {
+        [HttpPost]
+        public int GetPrimeNumber(int index)
+        {
+            return 0;
+        }
+    }
+}
