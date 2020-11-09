72a77c718fa56ea55f38b90080970148107f1d30
Guilherme Murari <guilherme.murari@moondu.io>
2020-11-09 20:06:34 +0000

    DI context startup and connection string

--- src/PrimeNumber.Api/Startup.cs
+++ src/PrimeNumber.Api/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PrimeNumber.Data.Data;
 
 namespace PrimeNumber.Api
 {
@@ -26,6 +28,13 @@ namespace PrimeNumber.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<PrimeNumberDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("PrimeNumberDbContext"));
+            });
+
+            services.AddScoped<PrimeNumberDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
