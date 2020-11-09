98dab7779f8d562ae0ccb09629ca29800764c5a0
Guilherme Murari <guilherme.murari@moondu.io>
2020-11-09 20:23:15 +0000

    Migrations

---
+++ src/PrimeNumber.Data/Migrations/20201109202055_initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimeNumber.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PrimeNums",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Index = table.Column<int>(nullable: false),
+                    PrimeValue = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrimeNums", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PrimeNums_Index",
+                table: "PrimeNums",
+                column: "Index",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PrimeNums");
+        }
+    }
+}
