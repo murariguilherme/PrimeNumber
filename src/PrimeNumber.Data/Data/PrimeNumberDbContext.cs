c568875142a5457f9f02afc97f9fde2daa00a17b
Guilherme Murari <guilherme.murari@moondu.io>
2020-11-09 19:30:09 +0000

    data layer dbcontext

---
+++ src/PrimeNumber.Data/Data/PrimeNumberDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumber.Data.Data
+{
+    public class PrimeNumberDbContext: DbContext
+    {
+        public PrimeNumberDbContext(DbContextOptions<PrimeNumberDbContext> options): base(options) { }
+    }
+}
