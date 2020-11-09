06646fb4ef69b5ab224769c2a544596577089a74
Guilherme Murari <guilherme.murari@moondu.io>
2020-11-09 20:09:59 +0000

    prime num repository

---
+++ src/PrimeNumber.Business/Interfaces/IRepositoryPrimeNum.cs
@@ -0,0 +1,11 @@
+﻿using PrimeNumber.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumber.Business.Interfaces
+{
+    public interface IRepositoryPrimeNum: IRepository<PrimeNum>
+    {
+    }
+}
---
+++ src/PrimeNumber.Data/Repository/RepositoryPrimeNum.cs
@@ -0,0 +1,13 @@
+﻿using PrimeNumber.Business.Models;
+using PrimeNumber.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumber.Data.Repository
+{
+    public class RepositoryPrimeNum: Repository<PrimeNum>
+    {
+        public RepositoryPrimeNum(PrimeNumberDbContext context): base(context) { }
+    }
+}
