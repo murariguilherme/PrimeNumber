fc826d9a17f27f5bad99efbd6b91d612d0e5a400
Guilherme Murari <guilherme.murari@moondu.io>
2020-11-09 19:32:07 +0000

    business layer application

---
+++ src/PrimeNumber.Business/Models/PrimeNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumber.Business.Models
+{
+    public class PrimeNumber
+    {
+        public int Index { get; set; }
+        public int PrimeValue { get; set; }
+    }
+}
